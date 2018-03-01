7fb2ae832f083204db47da99bccbbae5888d05e9
Itai Gendler <itai@codefresh.io>
2018-03-01 11:24:42 +0200

    update formula version v0.8.26

--- Formula/codefresh.rb
+++ Formula/codefresh.rb
@@ -1,9 +1,9 @@
 class Codefresh < Formula
     desc "Codefresh CLI provides a full and flexible interface to interact with Codefresh."
     homepage "http://cli.codefresh.io"
-    url "https://github.com/codefresh-io/cli/releases/download/v0.8.25/codefresh-v0.8.25-macos-x64.tar.gz"
-    version "v0.8.25"
-    sha256 "7960ab99eb51b9c8550949e693b298bb28f73bbed8f635f0a892a04cc912b848"
+    url "https://github.com/codefresh-io/cli/releases/download/v0.8.26/codefresh-v0.8.26-macos-x64.tar.gz"
+    version "v0.8.26"
+    sha256 "7865797c3d9366daf15d48c38dba30da058beb0bf249392abab98bcf620b8467"
   
     def install
       bin.install "codefresh"
