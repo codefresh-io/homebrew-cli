63db15796a3d4ae67ce9ed19844b0abe052917f4
cf-ci-bot-v2 <107364971+cf-ci-bot-v2@users.noreply.github.com>
2025-04-09 15:28:31 +0300

    update formula version to v0.2.5

--- Formula/cf2.rb
+++ Formula/cf2.rb
@@ -2,8 +2,8 @@ class Cf2 < Formula
   desc "Codefresh CLI tool, V2"
   homepage "https://codefresh.io/"
   url "https://github.com/codefresh-io/cli-v2.git",
-    tag:      "v0.2.4",
-    revision: "d37414cc5e46ce76e7d4f424be1eb05dcbf3910a"
+    tag:      "v0.2.5",
+    revision: "fe8f7259e83df1e076cdcc12abd13736421a623b"
   license "Apache-2.0"
 
   depends_on "go" => :build
