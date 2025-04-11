fd65a352537f50963d61e198d75ee43d04e5d8b1
cf-ci-bot-v2 <107364971+cf-ci-bot-v2@users.noreply.github.com>
2025-04-11 13:24:26 +0300

    update formula version v0.88.6

--- Formula/codefresh.rb
+++ Formula/codefresh.rb
@@ -3,7 +3,7 @@ class Codefresh < Formula
     homepage "http://cli.codefresh.io"
     url "https://github.com/codefresh-io/cli/releases/download/v0.88.6/codefresh-v0.88.6-macos-x64.tar.gz"
     version "v0.88.6"
-    sha256 "27303963dc200f677c9d9d3d12a4fa25e0686f131873118e18a9b988dab76578"
+    sha256 "6a0a443f810cb84380ca66d8d40e2f3ea09cb372a26d55f2c3165fc7a34649eb"
   
     def install
       bin.install "codefresh"
