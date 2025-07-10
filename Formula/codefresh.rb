964d9e86ad4b1b3a4918cc1cbb29d939698bcab5
cf-ci-bot-v2 <107364971+cf-ci-bot-v2@users.noreply.github.com>
2025-07-10 12:06:47 +0300

    update formula version v0.89.2

--- Formula/codefresh.rb
+++ Formula/codefresh.rb
@@ -1,9 +1,9 @@
 class Codefresh < Formula
     desc "Codefresh CLI provides a full and flexible interface to interact with Codefresh."
     homepage "http://cli.codefresh.io"
-    url "https://github.com/codefresh-io/cli/releases/download/v0.89.1/codefresh-v0.89.1-macos-x64.tar.gz"
-    version "v0.89.1"
-    sha256 "70912c0e4eb365817afab18f327943775adfafb578bdbba73742011b117f0dde"
+    url "https://github.com/codefresh-io/cli/releases/download/v0.89.2/codefresh-v0.89.2-macos-x64.tar.gz"
+    version "v0.89.2"
+    sha256 "7d423954effcb4cdefac29b27d63cbff700035845ab3aa6e23c042c3c8af2c3e"
   
     def install
       bin.install "codefresh"
